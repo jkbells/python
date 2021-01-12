21bbf6155ac228a04ddd774b340563e07e66f7b6
jawadkhan317 <73393333+jawadkhan317@users.noreply.github.com>
2021-01-13 00:25:54 +0500

    sum of the squares

---
+++ python04.py
@@ -0,0 +1,11 @@
+# the functions that calculates the sum of the squares
+
+def square(x):
+    return x*x
+
+def sum_square(x,y):
+    a=square(x)
+    b=square(y)
+    return a+b
+result = sum_square(2,3)
+result
