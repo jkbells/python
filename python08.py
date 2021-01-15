d5c7066f3508827111d2b943f55331084851f309
jawadkhan317 <73393333+jawadkhan317@users.noreply.github.com>
2021-01-15 15:34:42 +0500

    simple conditions

---
+++ python08.py
@@ -0,0 +1,18 @@
+
+# simple conditions
+
+def absolute_value(x):
+    """ compute abs(x) """
+    if x>0:
+        return x
+    elif x==0:
+        return 0
+    else:
+        return -x
+        
+   absolute_value(5)
+   absolute_value(0)
+   absolute_value(-5)
+   
+   
+        
