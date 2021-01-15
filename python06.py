a33f9fdd76272db1fff10ce7d36c1154690d5488
jawadkhan317 <73393333+jawadkhan317@users.noreply.github.com>
2021-01-15 15:33:45 +0500

    Global variable

---
+++ python06.py
@@ -0,0 +1,16 @@
+# some concepts about globel varaiables
+
+def f(x):
+    y=2
+    x=x+y
+    print ('x =',x)
+    print ('p =',p)
+    return x
+    
+p=22
+x=3
+y=6
+z=f(x)
+print ('z=',z)
+print ('x='  ,x)
+print ('y=' ,y)
