a60e3f3d81f0dce4720357f8756e49b4a064c3a9
jawadkhan317 <73393333+jawadkhan317@users.noreply.github.com>
2021-01-17 23:34:34 +0500

    nested if statements

---
+++ python09.py
@@ -0,0 +1,13 @@
+# we can also use nested if statements 
+
+a=6
+b=2
+if a>5:
+    if b>6:
+        print ("a is more than 5 and b is more than 6")
+    else:
+            print ("a is more than 5 but b is not more than b")
+else:
+    print ("a is not more than 5")
+    
+    print ("DONE")
